dcc4748db1829144c6208ad739dd8f686a34fb4f
mehmetaltuntas20444 <mehmetaltuntas@sabanciuniv.edu>
2021-07-30 00:05:12 +0300

    GameJam Runner

---
+++ Assets/Atakan/Scripts/Pencils.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pencils : MonoBehaviour
+{
+    public int listIndex;
+    public float scalepow;
+    public bool moving;
+    public Vector3 destiny;
+    public float movingTime;
+
+
+    private void Update()
+    {
+
+        if (GameManager.Instance.gameState == GameManager.GameState.InGame)
+        {
+            var scale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z-Time.deltaTime*scalepow);
+            ScaleAround(-transform.up,scale);
+            if(transform.localScale.z <= 0 || transform.position.y < 0)
+            {
+                kill();
+            }
+        }
+
+        if(moving)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, destiny, Time.deltaTime* movingTime );
+            //temptime += Time.deltaTime;
+            //if (temptime > movingTime) moving = false;
+        }
+    }
+    private void kill()
+    {
+        StackManager.Instance.popUp(listIndex);
+        Destroy(gameObject);
+        StackManager.Instance.sortPencils(2);
+
+    }
+
+    public void ScaleAround(Vector3 pivot, Vector3 newScale)
+    {
+        Vector3 A = transform.localPosition;
+        Vector3 B = pivot;
+ 
+        Vector3 C = A - B; // diff from object pivot to desired pivot/origin
+ 
+        float RS = newScale.x / transform.localScale.x; // relataive scale factor
+ 
+        // calc final position post-scale
+        Vector3 FP = B + C * RS;
+ 
+        // finally, actually perform the scale/translation
+        transform.localScale = newScale;
+        transform.localPosition = FP;
+    }
+
+    /* public void ScaleAround(GameObject target, Vector3 pivot, Vector3 newScale)
+     {
+     Vector3 A = target.transform.localPosition;
+     Vector3 B = pivot;
+ 
+     Vector3 C = A - B; // diff from object pivot to desired pivot/origin
+ 
+     float RS = newScale.x / target.transform.localScale.x; // relataive scale factor
+ 
+     // calc final position post-scale
+     Vector3 FP = B + C * RS;
+ 
+     // finally, actually perform the scale/translation
+     target.transform.localScale = newScale;
+     target.transform.localPosition = FP;
+     }
+     */
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cube" && GetComponent<StackTrigger>().inPlayer)
+        {
+            kill();
+        }
+        if(collision.gameObject.tag == "FinishLevel")
+        {
+            GameManager.Instance.gameState = GameManager.GameState.Finish;
+        }
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # RoketGamesGameJam
 Game Jam Project From Team Roket Games
+# Team Name
+Roket Games
+# Team Members Sort By First Letter
+Atakan Çakır
+Berfin Baytemur
+İlayda Akdemir
+Mehmet Altuntaş
